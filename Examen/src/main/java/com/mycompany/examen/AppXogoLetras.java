0bed30a4be71dfcaa1059f9033db584fa2301c06
David <99399340+A20DavicBC@users.noreply.github.com>
2022-06-03 11:04:53 +0200

    #1 - Añadido proyecto Examen

---
+++ Examen/src/main/java/com/mycompany/examen/AppXogoLetras.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.examen;
+
+import modelo.XogoAzar;
+
+/**
+ *
+ * @author A20DavidBC
+ */
+public class AppXogoLetras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(XogoAzar.XERADOR(100));
+    }
+    
+}
---
+++ Examen/src/main/java/modelo/Comprobable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author A20DavidBC
+ */
+public interface Comprobable {
+    public boolean comprobar(Object obj);
+}
