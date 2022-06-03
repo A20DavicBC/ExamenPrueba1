2350e5db3a7f75aae1542b2797fa7db7a44057cd
David <99399340+A20DavicBC@users.noreply.github.com>
2022-06-03 11:06:51 +0200

    #2 - Añadir proyecto Examen

---
+++ Examen/src/main/java/app/AppXogoLetras.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package app;
+
+import modelo.XogoAzar;
+
+/**
+ *
+ * @author A20DavidBC
+ */
+public class AppXogoLetras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
--- Examen/src/main/java/modelo/XogoLetras.java
+++ Examen/src/main/java/modelo/XogoLetras.java
@@ -4,10 +4,40 @@
  */
 package modelo;
 
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author A20DavidBC
  */
-public class XogoLetras {
+public final class XogoLetras extends XogoAzar {
+    
+    public static final String NOME_XOGO="Xogo de letras";
+    public static final String CONSOANTES="QWRTPSDFGHJKLÑZXCVBNM";
+    public static final String VOGAIS="AEIOU";
+    public static final int NUM_LETRAS=9;
+    
+    private Character[] letras;
+    private List<String> palabras;
+
+    public XogoLetras() {
+        super(NOME_XOGO);
+        
+        letras = new Character[NUM_LETRAS];
+        palabras = new ArrayList<>();
+        
+    }
+
+    public static int getVocal() {
+        return Random.nextInt(5);
+    }
+    
+    @Override
+    public boolean comprobar(Object obj) {
+        return true;
+    }
+    
     
 }
